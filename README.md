d012064ff08f027f3f47ad710f0e76ac9560220b
dylangriess <dylangriess@gmail.com>
2022-08-28 14:00:31 -0600

    updating license/badge function

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# generate-readme
+# README Generator
+
+## Link
+
+## https://github.com/dylangriess/generate-readme
+
+## Description
+
+This project was created to generate a high-quality, professional README.md file based on user input.
+I built this project to ease the process of generating a new README.md file for each homework project and future project I am working on.
+It solves the mundane task of creating each file from scratch.
+I learned how to use node.js using the Inquirer package.
+
+---
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Installation](#installation)
+- [Licensing](#licensing)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+---
+
+## Installation
+
+Node.js and Inquirer are required for this project installment.
+
+## Usage
+
+Users will input their data with prompts provided through the writeFile function.
+
+## Licensing
+
+This project is licensed under
+
+For details, please visit:
+
+## Contributing
+
+I learned how to use this application from the help of my professor, TA's, and classmates.
+
+## Tests
+
+N/A
+
+## Questions
+
+Visit my GitHub page: [https://github.com/dylangriess](https://github.com/https://github.com/dylangriess)
+
+Email me at: [dylangriess@gmail.com](dylangriess@gmail.com)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,69 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// function renderLicenseBadge(data) {
-// let licenseValue = data.license;
-// if (!licenseValue) {
-//   return "";
-// } else {
-//   return `${data.license}`;
-// }
-// }
+function renderLicenseBadge(data) {
+let licenseValue = "";
+if (licenseValue) {
+  licenseValue = `![License](https://img.shields.io/badge/License-${data.license}-blue.svg)`;
+}
+return licenseValue
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-// function renderLicenseLink(data)
+function renderLicenseLink(data)
+let licenseInput = data.license;
+// Switch statement based on user input for data.license
+switch (licenseInput) {
+  case "Apache License 2.0":
+    licenseLink = "https://opensource.org/licenses/Apache-2.0";
+    break;
+  case "GNU GPLv3":
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+    break;
+  case "GNU GPLv2":
+    licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"
+    break;
+  case "MIT":
+    licenseLink ="https://opensource.org/licenses/MIT)";
+    break;
+  case "ISC":
+    licenseLink = "https://opensource.org/licenses/ISC";
+    break;
+  default:
+    licenseLink = "";
+}
+return licenseLink;
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {
+
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
-## Link
+${renderLicenseBadge(data.license)}
+---
+## Link   
 ${data.urllink}
----	
-## Description 
 
+---
+## Description    
 ${data.description1}
 ${data.description2}
 ${data.description3}
 ${data.description4}
----
-## Table of Contents
 
-  ✦ [Usage](#Usage)
-  ✦ [Installation](#Installation)
-  ✦ [Licensing](#Licensing)
-  ✦ [Contributing](#Contributing)
-  ✦ [Tests](#Tests)
-  ✦ [Questions](#questions)
 ---
-## Installation
+## Table of Contents   
+
+- [Usage](#usage)
+- [Installation](#installation)
+- [Licensing](#licensing)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+---
+## Installation   
 
 ${data.installation}
 ---
-## Usage
+## Usage   
 
 ${data.usage}
 ---
-## Licensing
+## Licensing   
 
-This project is licensed under   
+This project is licensed under ${data.license}
 
-For details, please visit: 
+For details, please visit: ${renderLicenseLink(data)}
 ---
-## Contributing
+## Contributing   
 
 ${data.credits}
 ---
-## Tests
+## Tests   
 
 ${data.testing}
 ---
-## Questions
+## Questions   
 
 Visit my GitHub page: [${data.github}](https://github.com/${data.github})   
 
