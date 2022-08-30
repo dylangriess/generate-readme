1bbb6d26c0f181c07bc7fe0fe249289eb43624d8
dylangriess <dylangriess@gmail.com>
2022-08-29 18:14:17 -0600

    fixing function for renderLicenseBadge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(data) {
-let licenseValue = "";
-if (licenseValue) {
-  licenseValue = `![License](https://img.shields.io/badge/License-${data.license}-blue.svg)`;
-}
-return licenseValue
+function renderLicenseBadge(license) {
+  let licenseValue = license;
+  console.log(licenseValue);
+  let message = "";
+  let color = "";
+  let renderBadge = "";
+  // Switch statement generates value of label, message, color & adds them to URL
+  switch (licenseValue) {
+    case "Apache License 2.0":
+      message = "Apache_2.0";
+      color = "yellowgreen";
+      renderBadge = `![License](https://img.shields.io/badge/license-${message}-${color})`;
+      break;
+    case "GNU GPL v3":
+      message = "GPLv3";
+      color = "blue";
+      renderBadge = `![License: GPL v3](https://img.shields.io/badge/license-${message}-${color})`;
+      break;
+    case "GNU GPL v2":
+      message = "GPL_v2";
+      color = "blue";
+      renderBadge = `![License: GPL v2](https://img.shields.io/badge/license-${message}-${color})`;
+      break;
+    case "MIT":
+      message = "MIT";
+      color = "yellow";
+      renderBadge = `![License: MIT](https://img.shields.io/badge/license-${message}-${color})`;
+      break;
+    case "ISC":
+      message = "ISC";
+      color = "blue";
+      renderBadge = ` ![License: ISC](https://img.shields.io/badge/license-${message}-${color})`;
+      break;
+    default:
+      return "No license selected.";
+  }
+  return renderBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(data)
-let licenseInput = data.license;
-// Switch statement based on user input for data.license
-switch (licenseInput) {
-  case "Apache License 2.0":
-    licenseLink = "https://opensource.org/licenses/Apache-2.0";
-    break;
-  case "GNU GPLv3":
-    licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
-    break;
-  case "GNU GPLv2":
-    licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"
-    break;
-  case "MIT":
-    licenseLink ="https://opensource.org/licenses/MIT)";
-    break;
-  case "ISC":
-    licenseLink = "https://opensource.org/licenses/ISC";
-    break;
-  default:
-    licenseLink = "";
+function renderLicenseLink(data) {
+  let licenseInput = data.license;
+  // Switch statement based on user input for data.license
+  switch (licenseInput) {
+    case "Apache License 2.0":
+      licenseLink = "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "GNU GPLv3":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+    case "GNU GPLv2":
+      licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+      break;
+    case "MIT":
+      licenseLink = "https://opensource.org/licenses/MIT";
+      break;
+    case "ISC":
+      licenseLink = "https://opensource.org/licenses/ISC";
+      break;
+    default:
+      licenseLink = "";
+  }
+  return licenseLink;
 }
-return licenseLink;
-
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -48,14 +79,14 @@ function generateMarkdown(data) {
 ${renderLicenseBadge(data.license)}
 ---
 ## Link   
-${data.urllink}
+>${data.urllink}
 
 ---
 ## Description    
-${data.description1}
-${data.description2}
-${data.description3}
-${data.description4}
+>${data.description1}
+>${data.description2}
+>${data.description3}
+>${data.description4}
 
 ---
 ## Table of Contents   
@@ -68,33 +99,25 @@ ${data.description4}
 - [Questions](#questions)
 
 ---
-## Installation   
-
-${data.installation}
+## Installation    
+>${data.installation}
 ---
-## Usage   
-
-${data.usage}
+## Usage    
+>${data.usage}
 ---
-## Licensing   
-
-This project is licensed under ${data.license}
-
-For details, please visit: ${renderLicenseLink(data)}
+## Licensing    
+>This project is licensed under ${data.license}.
+>For details, please visit: ${renderLicenseLink(data)}
 ---
-## Contributing   
-
-${data.credits}
+## Contributing    
+>${data.credits}
 ---
-## Tests   
-
-${data.testing}
+## Tests    
+>${data.testing}
 ---
-## Questions   
-
-Visit my GitHub page: [${data.github}](https://github.com/${data.github})   
-
-Email me at: [${data.email}](${data.email})
+## Questions    
+>Visit my GitHub page: [${data.github}](https://github.com/${data.github})   
+>Email me at: [${data.email}](${data.email})
 `;
 }
 
